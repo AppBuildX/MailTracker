51b3ee81c2cfc2bf26db36e101232b3637ac2116
AppBuildX <119816299+AppBuildX@users.noreply.github.com>
2023-05-29 13:43:18 +0530

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,23 +8,7 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ],
-  callbacks: {
-    signIn: async ({ account }) => {
-      try {
-        console.log("sasa");
-        const { id_token } = account;
-        const response = await axiosInstance.post("/api/auth/google/callback", {
-          idToken: id_token
-        });
-        console.log("1234");
-        const { auth } = response.data;
-        return auth ? true : "/";
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
+  ]
 };
 
 export default NextAuth(authOptions);
